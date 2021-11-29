d797d6ec101aa2181657cdf7c9c94fb49c08adcb
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2021-11-30 00:42:42 +0300

    Поставлен \2023 в content

--- src/js/script.js
+++ src/js/script.js
@@ -33,7 +33,7 @@ window.addEventListener(
 
         // Добавление динамической подсветки кнопок при прокрутке через обзервер
         const observer = new IntersectionObserver(onIntersect, {
-            threshold: [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+            threshold: [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
         });
 
         const targets = document.querySelectorAll('.observe');
